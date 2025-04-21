6052557f9897402f1e775144d19400f3a3e74253
YounesAH15 <younesharrat5896@gmail.com>
2025-04-21 20:56:12 +0200

    structure of introduction done no styling

---
+++ src/components/ExplainerVideo.svelte
@@ -0,0 +1,20 @@
+<!-- src/lib/components/ExplainerVideo.svelte -->
+<!-- svelte-ignore element_invalid_self_closing_tag -->
+
+<script lang="ts">
+    // import myVideo from '$lib/assets/my-video.mp4';
+    import myVideo from '$lib/assets/videos/chess_anime_player.mp4';
+    // /videos/chess_anime_player.mp4
+  </script>
+  
+
+<video
+  controls={true}
+  preload=""
+  poster="src\static\images\explainer-thumbnail.jpg"
+  muted={false}
+  class="rounded-xl shadow-lg w-3/5 h-3/5 max-w-4/5 max-h-4/5 mx-auto"
+  src={myVideo}
+>
+  Your browser does not support the video tag.
+</video>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import ExplainerVideo from "../components/ExplainerVideo.svelte";
+	import VideoModal from "../components/VideoModal.svelte";
+    import myVideo from '$lib/assets/videos/chess_anime_player.mp4';
+
+    let explainerSrc = ""
+</script>
+<section>
+
+    <h1>VEGE</h1>
+    <h1>Virtual Escape Game Experience</h1>
+</section>
+
+<section>
+    <!-- Auth forms, lucia or oauth2 using google -->
+     <h1>AUTH
+        THEN
+     </h1>
+</section>
+
+<section>
+    <!-- Just an Explanation For the Game, through a video or a text -->
+     <h1>Explainer</h1>
+    <VideoModal videoSrc={myVideo} open={true} onClose={()=>{}}></VideoModal>
+</section>
+
+<section>
+    <h1>Ready to Start ? </h1>
+    
+</section>
+
